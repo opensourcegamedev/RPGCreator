f75965ca879a8e2ee50a1b2ff4701f8c71d8e6cb
JuKu <kuenzel.justin@t-online.de>
2017-05-30 18:08:13 +0200

    changed create project dialog.

--- editor/src/main/java/com/jukusoft/rpgcreator/editor/javafx/controller/CreateProjectDialogController.java
+++ editor/src/main/java/com/jukusoft/rpgcreator/editor/javafx/controller/CreateProjectDialogController.java
@@ -1,10 +1,17 @@
 package com.jukusoft.rpgcreator.editor.javafx.controller;
 
 import com.jukusoft.rpgcreator.engine.javafx.FXMLController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -17,6 +24,11 @@ import java.util.ResourceBundle;
  */
 public class CreateProjectDialogController implements FXMLController, Initializable {
 
+    protected int DEFAULT_PORT = 1234;
+
+    protected final String CSS_FAILED_STYLE = "-fx-border-color:#ff0000; -fx-background-color:#c4cfed; ";
+    protected final String CSS_SUCCESS_STYLE = "-fx-border-color:#1b52f1; -fx-background-color:#c4cfed; ";
+
     @FXML
     protected TextField projectTitleTextField;
 
@@ -26,9 +38,38 @@ public class CreateProjectDialogController implements FXMLController, Initializa
     @FXML
     protected TextField serverIPTextField;
 
+    @FXML
+    protected TextField serverPortTextField;
+
+    @FXML
+    protected CheckBox includeAssetsCheckBox;
+
+    @FXML
+    protected Button createGameButton;
+
     @Override
     public void init(Stage stage, Scene scene, Pane pane) {
         projectPathTextField.setText(getBasePath());
+
+        createGameButton.setOnMouseEntered((MouseEvent event) -> {
+            //check, if form is valide
+            if (validate()) {
+                //create project
+                create();
+            }
+        });
+
+        projectTitleTextField.setOnKeyTyped((KeyEvent event) -> {
+            generateAndSetProjectPath();
+
+            validate();
+        });
+
+        projectPathTextField.setOnMouseMoved((MouseEvent event) -> {
+            generateAndSetProjectPath();
+        });
+
+        serverPortTextField.setText(DEFAULT_PORT + "");
     }
 
     @Override
@@ -38,6 +79,42 @@ public class CreateProjectDialogController implements FXMLController, Initializa
 
     protected String getBasePath () {
         return new File("./games/").getAbsolutePath().replace(".\\", "").replace("\\", "/");
+    }
+
+    /**
+    * check, if all form fields are valide
+     *
+     * @return true, if all fields are valide
+    */
+    protected boolean validate () {
+        //TODO: add code here
+
+        //get project title
+        String title = projectTitleTextField.getText();
+
+        if (!title.equals(title.replace(" ", ""))) {
+            projectTitleTextField.setStyle(CSS_FAILED_STYLE);
+            return false;
+        } else {
+            projectTitleTextField.setStyle(CSS_SUCCESS_STYLE);
+        }
+
+        return true;
+    }
+
+    /**
+    * create new project
+    */
+    protected void create () {
+        //
+    }
+
+    protected void generateAndSetProjectPath() {
+        String text = getBasePath() + "/" + projectTitleTextField.getText();
+
+        if (!projectPathTextField.getText().equals(text)) {
+            projectPathTextField.setText(getBasePath() + "/" + projectTitleTextField.getText());
+        }
     }
 
 }
