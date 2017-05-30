e13f635c7b90436a77110e2dd659fb5410a74b4d
JuKu <kuenzel.justin@t-online.de>
2017-05-30 17:33:09 +0200

    added support to set current workdir path into create project dialog.

--- editor/src/main/java/com/jukusoft/rpgcreator/editor/javafx/controller/CreateProjectDialogController.java
+++ editor/src/main/java/com/jukusoft/rpgcreator/editor/javafx/controller/CreateProjectDialogController.java
@@ -1,11 +1,14 @@
 package com.jukusoft.rpgcreator.editor.javafx.controller;
 
 import com.jukusoft.rpgcreator.engine.javafx.FXMLController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,14 +17,27 @@ import java.util.ResourceBundle;
  */
 public class CreateProjectDialogController implements FXMLController, Initializable {
 
+    @FXML
+    protected TextField projectTitleTextField;
+
+    @FXML
+    protected TextField projectPathTextField;
+
+    @FXML
+    protected TextField serverIPTextField;
+
     @Override
     public void init(Stage stage, Scene scene, Pane pane) {
-        //
+        projectPathTextField.setText(getBasePath());
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //
+    }
+
+    protected String getBasePath () {
+        return new File("./games/").getAbsolutePath().replace(".\\", "").replace("\\", "/");
     }
 
 }
