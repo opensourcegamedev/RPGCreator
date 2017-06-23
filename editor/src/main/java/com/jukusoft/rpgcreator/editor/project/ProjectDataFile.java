9b75e32f3cdc9c94d445b4d9ce19211a2502cba2
JuKu <kuenzel.justin@t-online.de>
2017-06-23 17:44:10 +0200

    added documentation.

---
+++ editor/src/main/java/com/jukusoft/rpgcreator/editor/project/ProjectDataFile.java
@@ -0,0 +1,19 @@
+package com.jukusoft.rpgcreator.editor.project;
+
+import java.io.File;
+
+/**
+ * Created by Justin on 23.06.2017.
+ */
+public class ProjectDataFile {
+
+    //file extension for RPC Creator Project Definition file
+    protected static final String PROJECT_FILE_EXTENSION = "rcp";
+
+    protected String
+
+    public ProjectDataFile (File file) {
+        //
+    }
+
+}
